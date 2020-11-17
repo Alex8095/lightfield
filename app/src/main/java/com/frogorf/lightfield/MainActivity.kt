af8b0b707f9a6ccd151a32fa74c2888aa346a6a5
alexandr.ts <alexandr.ts@mail.int>
2020-11-17 16:55:36 +0200

    project setup

---
+++ app/src/main/java/com/frogorf/lightfield/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.frogorf.lightfield
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
